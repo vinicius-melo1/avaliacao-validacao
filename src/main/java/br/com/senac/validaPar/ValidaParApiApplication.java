1df46bb046635643608f992e5d892e5916b5a090
VINICIUS NASCIMENTO DE MELO <vinicius.melo2@alunos.sc.senac.br>
2026-03-19 19:23:21 -0300

    Primeiro commit

---
+++ src/main/java/br/com/senac/validaPar/ValidaParApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.senac.validaPar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidaParApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidaParApiApplication.class, args);
+	}
+
+}
